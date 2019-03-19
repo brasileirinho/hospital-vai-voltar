cb4695589e2ffe9b0a72593f23b0bc60a641c53c
Francisco Chaves <francisco.jschaves@gmail.com>
2019-03-19 11:17:50 -0300

    remoção imagem causa 'unfortunately has stopped' e adição da navegação main activity

--- app/src/main/java/br/com/franciscochaves/hospitalvaivoltar/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/hospitalvaivoltar/MainActivity.java
@@ -1,10 +1,12 @@
 package br.com.franciscochaves.hospitalvaivoltar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -15,6 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
         this.mViewHolder.buttonAdiconarPaciente = findViewById(R.id.button_adicionar_paciente);
         this.mViewHolder.buttonPesquisarPaciente = findViewById(R.id.button_pesquisa_paciente);
+
+        this.mViewHolder.buttonAdiconarPaciente.setOnClickListener(this);
+        this.mViewHolder.buttonPesquisarPaciente.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.button_adicionar_paciente){
+            Intent intent = new Intent(this, AddPacienteActivity.class);
+            startActivity(intent);
+        }else if(id == R.id.button_pesquisa_paciente){
+            Intent intent = new Intent(this, DetalheActivity.class);
+            startActivity(intent);
+        }
     }
 
     private static class ViewHolder {
