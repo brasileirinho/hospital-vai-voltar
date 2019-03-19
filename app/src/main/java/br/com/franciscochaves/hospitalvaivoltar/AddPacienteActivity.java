ae910ce135dfb142a99f41f2799722bdd792bb7a
Francisco Chaves <francisco.jschaves@gmail.com>
2019-03-19 14:24:45 -0300

    Uilizando o SQLLite com Banco de dados

--- app/src/main/java/br/com/franciscochaves/hospitalvaivoltar/AddPacienteActivity.java
+++ app/src/main/java/br/com/franciscochaves/hospitalvaivoltar/AddPacienteActivity.java
@@ -1,14 +1,21 @@
 package br.com.franciscochaves.hospitalvaivoltar;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class AddPacienteActivity extends AppCompatActivity {
+public class AddPacienteActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder mViewHolder = new ViewHolder();
+
+    private SQLiteDatabase db;
+    private String nomeDB = "DBHospital";
+    private String nomeTabela = "Pacientes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,50 @@ public class AddPacienteActivity extends AppCompatActivity {
         this.mViewHolder.batimentoCardiaco = findViewById(R.id.text_batimento_cardiaco);
         this.mViewHolder.temperatura = findViewById(R.id.text_temperatura);
         this.mViewHolder.salvar = findViewById(R.id.button_salvar);
+
+        this.mViewHolder.salvar.setOnClickListener(this);
+
+        db = openOrCreateDatabase(nomeDB, Context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS "+ nomeTabela +
+                "(Nome VARCHAR," +
+                " Leito VARCHAR," +
+                " PressaoArterial VARCHAR," +
+                " BatimentoCardiacos VARCHAR," +
+                " Temperatura VARCHAR )"
+        );
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.button_salvar){
+            if(verificarCamposPreenchido()){
+
+                db.execSQL("INSERT INTO " + nomeTabela +
+                        " VALUES ('"+mViewHolder.nome.getText()+
+                        "','"+mViewHolder.leito.getText()+
+                        "','"+mViewHolder.pressaoArterial.getText()+
+                        "','"+mViewHolder.batimentoCardiaco.getText()+
+                        "','"+mViewHolder.temperatura.getText()+
+                        "');"
+                );
+
+                exixbirMessagem("Ok", "Dados Gravados com sucesso!");
+            }else{
+                exixbirMessagem("Erro", "Preencha todos os campos");
+            }
+        }
+    }
+
+    private boolean verificarCamposPreenchido() {
+        if(mViewHolder.nome.getText().toString().trim().length() == 0 ||
+            mViewHolder.leito.getText().toString().trim().length() == 0 ||
+            mViewHolder.pressaoArterial.getText().toString().trim().length() == 0 ||
+            mViewHolder.batimentoCardiaco.getText().toString().trim().length() == 0 ||
+            mViewHolder.temperatura.getText().toString().trim().length() == 0){
+            return false;
+        }
+        return true;
     }
 
     private static class ViewHolder {
