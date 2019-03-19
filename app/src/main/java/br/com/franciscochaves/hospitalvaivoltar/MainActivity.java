5f0fa818b05fad83db5040110f68422fe8031f88
Francisco Chaves <francisco.jschaves@gmail.com>
2019-03-19 01:44:14 -0300

    Adicionando o pradão ViewHolder na main activity

--- app/src/main/java/br/com/franciscochaves/hospitalvaivoltar/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/hospitalvaivoltar/MainActivity.java
@@ -2,12 +2,23 @@ package br.com.franciscochaves.hospitalvaivoltar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.buttonAdiconarPaciente = findViewById(R.id.button_adicionar_paciente);
+        this.mViewHolder.buttonPesquisarPaciente = findViewById(R.id.button_pesquisa_paciente);
+    }
+
+    private static class ViewHolder {
+        Button buttonAdiconarPaciente;
+        Button buttonPesquisarPaciente;
     }
 }
